0cb63a540c6ac75fd2b2424846d22a157a90dff6
BiagioCozzolino <biagiocozzolino1996@gmail.com>
2022-06-10 15:40:04 +0200

    Effettuato Bonus, commentato

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -4,45 +4,49 @@ import java.util.Scanner;
 
 public class CheckGuest {
 	public static void main(String[] args) {
-		
-		//Inizio array e variabili
-		String[] listaInvitati = {
-				"Dua Lipa",
-				"Paris Hilton",
-				"Manuel Agnelli",
-				"J-Ax",
-				"Francesco Totti",
-				"Ilary Blasi",
-				"Bebe Vio",
-				"Luis",
-				"Pardis Zarei",
-				"Martina Maccherone",
-				"Rachel Zeilic"
-		};
+
+		// Inizio array e variabili
+		String[] listaInvitati = { "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti",
+				"Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic" };
 		String nomeInserito;
-		
-		//Inizio sezione immissione nome
+
+		// Inizio sezione immissione nome
 		Scanner scan = new Scanner(System.in);
-		
+
 		System.out.println("Benvenuto alla festa dei Ferragnez!, prego dimmi il tuo nome per verificare l'invito : ");
 		nomeInserito = scan.nextLine();
-		
-		//Inizio sezione verifica invitato
+
+		// Inizio sezione verifica invitato
 		boolean invitato = false;
 		int counter = 0;
-		
-		while (invitato == false && counter < listaInvitati.length) {
-			if (listaInvitati[counter].equalsIgnoreCase(nomeInserito)){
-				invitato = true;
-				System.out.println("Salve Sing./Sig.ra " + nomeInserito +" lei può entrare alla festa dei Ferragnez!");
-				
-			}else {
-				counter++;
-			}
-		}
-		if(!invitato) {
-			System.out.println("Mi dispiace lei non risulta invitato alla festa dei Ferragnez, è pregato di andarsene.");
 
+		/*
+		 * while (invitato == false && counter < listaInvitati.length) { if
+		 * (listaInvitati[counter].equalsIgnoreCase(nomeInserito)){ invitato = true;
+		 * System.out.println("Salve Sing./Sig.ra " + nomeInserito
+		 * +" lei può entrare alla festa dei Ferragnez!");
+		 * 
+		 * }else { counter++; } } 
+		 * if(!invitato) { System.out.
+		 * println("Mi dispiace lei non risulta invitato alla festa dei Ferragnez, è pregato di andarsene."
+		 * );
+		 * 
+		 * }
+		 */
+
+		// Bonus con For
+
+		for (counter = 0; counter < listaInvitati.length; counter++) {
+
+			if (listaInvitati[counter].equalsIgnoreCase(nomeInserito)) {
+				invitato=true;
+				System.out.println("Salve Sing./Sig.ra " + nomeInserito + " lei può entrare alla festa dei Ferragnez!");
+			}
+				 
+
+		}
+		if(!invitato) { 
+			System.out.println("Mi dispiace lei non risulta invitato alla festa dei Ferragnez, è pregato di andarsene.");
 		}
 	}
 }

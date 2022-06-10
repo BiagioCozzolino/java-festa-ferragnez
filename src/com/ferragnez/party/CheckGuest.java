105b8e9df929b1f709bbd52895bf8ef718b29c19
BiagioCozzolino <biagiocozzolino1996@gmail.com>
2022-06-10 15:06:19 +0200

    Creato Array e primo while per verifica lista

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,5 +1,41 @@
 package com.ferragnez.party;
 
-public class CheckGuest {
+import java.util.Scanner;
 
+public class CheckGuest {
+	public static void main(String[] args) {
+
+		String[] listaInvitati = {
+				"Dua Lipa",
+				"Paris Hilton",
+				"Manuel Agnelli",
+				"J-Ax",
+				"Francesco Totti",
+				"Ilary Blasi",
+				"Bebe Vio",
+				"Luis",
+				"Pardis Zarei",
+				"Martina Maccherone",
+				"Rachel Zeilic"
+		};
+		String nomeInserito;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Benvenuto alla festa dei Ferragnez!, prego dimmi il tuo nome per verificare l'invito : ");
+		nomeInserito = scan.nextLine();
+		
+		boolean invitato = false;
+		int counter = 0;
+		while (invitato == false && counter < listaInvitati.length) {
+			if (listaInvitati[counter].equals(nomeInserito)){
+				invitato = true;
+				System.out.println("Salve Sing./Sig.ra " + nomeInserito +" lei può entrare alla festa dei Ferragnez!");
+				
+			}else {
+				counter++;
+			}
+		}
+		
+	}
 }
